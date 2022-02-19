af41aac926cf7ef0adf140a50ddf765b3745096d
myname <myemail@myemail.com>
2022-02-20 04:13:05 +0800

    First version

---
+++ OoiWorld/src/OoiWorld.java
@@ -0,0 +1,9 @@
+
+public class OoiWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first Ooi World Java Program");
+	}
+
+}
